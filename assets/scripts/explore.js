daac34b4e7e88eaeb5c0dddfc9b1558f48a995ec
Kara Hoagland <khoaglan@ucsd.edu>
2022-10-30 16:45:20 -0700

    explorenexpose

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,51 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+var speaker = window.speechSynthesis;
+var text = document.getElementById('text-to-speak');
+var voiceSel = document.getElementById('voice-select');
+var face = document.querySelector('img');
+var currentVoice;
+
 function init() {
-  // TODO
+  const playBtn = document.querySelector('button');
+  playBtn.addEventListener('click', playSpeech);
+  speaker.addEventListener('voiceschanged', fillVoices);
+  voiceSel.addEventListener('change', voiceSwitch);
+  setInterval(facial, 5);
+}
+
+function fillVoices(){
+  var voices = speaker.getVoices();
+  for (let i = 0; i < voices.length; i++){
+    const opt = document.createElement('option');
+    opt.textContent = voices[i].name;
+    voiceSel.appendChild(opt);
+  }
+}
+
+function voiceSwitch(option){
+  var voices = speaker.getVoices();
+  console.log(option.target.value);
+  for(let i = 0; i < voices.length; i++){
+    if(voices[i].name === option.target.value){
+      console.log(voices[i].name);
+      currentVoice = voices[i];
+    }
+  }
+}
+
+function playSpeech(option){
+  const utterance = new SpeechSynthesisUtterance(text.value);
+  utterance.voice = currentVoice;
+  speaker.speak(utterance);
+}
+
+function facial(){
+  if(speaker.speaking){
+    face.src = "assets/images/smiling-open.png";
+  }
+  else{
+    face.src = "assets/images/smiling.png";
+  }
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,66 @@
 // expose.js
 
 window.addEventListener('DOMContentLoaded', init);
+var hornImg = document.querySelector('img');
+var hornAud = document.querySelector('audio');
+var volumeImg = document.querySelector('div > img');
+var state = 'none';
+const jsConfetti = new JSConfetti();
 
 function init() {
-  // TODO
+  const hornSel = document.getElementById('horn-select');
+  const volRange = document.getElementById('volume');
+  const playBtn = document.querySelector('button');
+  /*var hornImg = document.querySelector('img');
+  var hornAud = document.getElementsByClassName('hidden');
+  var volumeImg = document.querySelector('div' > 'img');*/
+  hornSel.addEventListener('change', hornChange);
+  volRange.addEventListener('input', volChange);
+  playBtn.addEventListener('click', playSound);
+
+}
+
+function hornChange(horn) {
+  console.log
+  state = horn.target.value;
+  switch (horn.target.value){
+    case 'air-horn':
+      hornImg.src = "assets/images/air-horn.svg";
+      hornAud.src = "assets/audio/air-horn.mp3";
+      break;
+    case 'car-horn':
+      hornImg.src = "assets/images/car-horn.svg";
+      hornAud.src = "assets/audio/car-horn.mp3";
+      break;
+    case 'party-horn':
+      hornImg.src = "assets/images/party-horn.svg";
+      hornAud.src = "assets/audio/party-horn.mp3";
+      break;
+    default:
+      hornImg.src = "assets/images/no-image.png";
+      hornAud.src = "";
+  }
+}
+
+function volChange(vol){
+  hornAud.volume = vol.target.value/100;
+  if(vol.target.value >= 67){
+    volumeImg.src = "assets/icons/volume-level-3.svg";
+  }
+  else if(vol.target.value >= 33){
+    volumeImg.src = "assets/icons/volume-level-2.svg";
+  }
+  else if(vol.target.value >= 1){
+    volumeImg.src = "assets/icons/volume-level-1.svg";
+  }
+  else{
+    volumeImg.src = "assets/icons/volume-level-0.svg";
+  }
+}
+
+function playSound(){
+  hornAud.play();
+  if(state == 'party-horn'){
+    jsConfetti.addConfetti();
+  }
 }
